612bfb5ca40aed0de69c100109acd61ebea7f3d8
jrohan1 <j.rohan1@nuigalway.ie>
2018-03-08 13:36:42 +0000

    Change to button color
    button color on hover changed

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -2,11 +2,14 @@ jQuery(document).ready(function($) {
   $(window).scroll(function() {
     var scrollPos = $(window).scrollTop();
     var navbar = $('.navigation');
+    var button = $('.specialButton');
 
     if (scrollPos > 500) {
       navbar.addClass('alt-color');
+      button.addClass('alt-border')
     } else {
       navbar.removeClass('alt-color');
+      button.removeClass('alt-border');
     }
   });
 });

25b795183914ca7aa9eb40e3508939d29feba4fc
Alan Flaherty <aflaherty13@github.com>
2018-03-11 14:36:51 +0000

    Removed un-necessary model classes in the data/ directory.

--- data/models/item.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var cafe = require('../models/cafe');
-require('./util');
-
-var itemSchema = new Schema({
-    name: String,
-    seller:
-});

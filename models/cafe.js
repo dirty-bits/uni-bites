9afc084dc67ed1314f6a073f1b4b963853f81c9b
Alan Flaherty <aflaherty13@github.com>
2018-03-20 19:18:04 +0000

    Updated the cafe.js model to include the urlTag field, without this the database update scripts were not including the urlTag for the cafes.

--- models/cafe.js
+++ models/cafe.js
@@ -7,7 +7,8 @@ require('./util');
 
 const cafeSchema = new mongoose.Schema({
     name: { type: String },
-    location: { type: String }
+    location: { type: String },
+    urlTag: { type: String }
 });
 
 module.exports = mongoose.model('unibites-cafes', cafeSchema);

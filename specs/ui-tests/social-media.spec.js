f75cb879b06874f6efdde3cec51b6f5378d266d2
elliebobellie <23409382+elliebobellie@users.noreply.github.com>
2018-04-16 12:32:57 +0100

    social media buttons in footer test
    UI test for social media buttons in footer actually link to social media pages. (Might have to change sleep wait time)

---
+++ specs/ui-tests/social-media.spec.js
@@ -0,0 +1,35 @@
+/* This tests the buttons in footer link to social media. (Tests from homepage)*/
+
+describe('uni-bites about tests', () => {
+    const until = protractor.ExpectedConditions;
+  
+    beforeEach(() => {
+        browser.waitForAngularEnabled(false);
+        browser.get(`${browser.params.baseUrl}`); //rework to page pattern see : http://blog.scottlogic.com/2015/11/06/ProtractorForBeginnersPart1.html
+    
+        fbIcon = element(by.id('facebookIcon'));
+        tweetIcon = element(by.id('twitterIcon'));
+        instaIcon = element(by.id('instagramIcon'));
+    });
+
+    it('should link to facebook page', () => {
+        fbIcon.click();
+        browser.sleep(2000);
+        expect(browser.getCurrentUrl()).toBe('https://www.facebook.com/Uni-Bites-2463469143877653/?ref=page_internal');
+    });
+
+    it('should link to twitter page', () => {
+        tweetIcon.click();
+        browser.sleep(2000);
+        expect(browser.getCurrentUrl()).toBe('https://twitter.com/BitesUni');
+    });
+    
+    it('should link to instragram page', () => {
+        instaIcon.click();
+        browser.sleep(2000);
+        expect(browser.getCurrentUrl()).toBe('https://www.instagram.com/bitesuni/');
+    });
+    
+    afterEach(() => {
+    });
+});

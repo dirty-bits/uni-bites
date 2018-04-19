27791b195d15fe827201150087110972a10ca30f
Daniel McFadden <dmcfaddengalway@gmail.com>
2018-04-19 12:50:20 +0100

    added comments to index so they show up on front page

--- public/javascripts/cafe.js
+++ public/javascripts/cafe.js
@@ -12,10 +12,10 @@ $(document).ready(() => {
 
 $('#postForm').submit((event) => {
     event.preventDefault();
-    $.post('/api/addComment', 
-        { 
-            comment: event.target.inputPost.value, 
-            rating: $("input[name='rating']:checked").val() 
+    $.post('/api/addComment',
+        {
+            comment: event.target.inputPost.value,
+            rating: $("input[name='rating']:checked").val()
         }, (result) => {
         $('#charRemaining').html(totalCharacters);
 
@@ -40,23 +40,34 @@ function getStarsHtml(rating){
 
 function getComments() {
     $.get('/api/getComments', (data) => {
-        let posts = '<div class="col-sm-12 "><p class="title">Reviews</p></div>';
+        let posts = '<div class="col-sm-12"><p class="title">Reviews</p></div>';
+        let recentReviews = '<div class="col-lg-6"><h1 class="headline alignText">Most Recent Reviews</h1></div>';
+
         for(let i = 0; i < data.length; i++) {
-            if(typeof(data[i].rating) == "undefined"){
+            if(typeof(data[i].rating) == "undefined") {
                 data[i].rating = 1;
             }
 
-            posts += 
-`<div class="col-sm-4">
-    <p>${data[i].user_name}</p>
-    <p>"${data[i].comment}"</p>
-` + getStarsHtml(data[i].rating) + `
-    <button type='button' name='${data[i]._id}' class='btn btn-danger'>Delete</button>
-</div>`;
+            posts +=
+                `<div class="col-sm-4">
+                    <p>${data[i].user_name}</p>
+                    <p>"${data[i].comment}"</p>
+                ` + getStarsHtml(data[i].rating) + `
+                </div>`;
+        }
+
+        for(let i = 0; i < 4; i++) {
+            recentReviews +=
+                `<div class="col-lg-3 col-xs-7 col-lg-offset-0 col-xs-offset-2 toRight">
+                    <h1 class="subHeaderLower">${data[i].user_name}</h1>
+                    <p class="captionLower">${data[i].comment}</p>
+                </div>`;
         }
 
         $('#cafereviews').html(posts);
         $('#count').html(data.length);
+
+        $('#recentReviews').html(recentReviews);
 
         if(!showPosts) {
             $('#feedPosts').hide();
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,37 @@
+$(document).ready(() => {
+    $('#postForm').keyup((event) => {
+        const inputText = event.target.value;
+        $('#charRemaining').html(totalCharacters - inputText.length);
+    });
+
+    getComments();
+});
+
+function getComments() {
+    $.get('/api/getComments', (data) => {
+        let recentReviews = '';
+
+        for(let i = 0; i < 3; i++) {
+            recentReviews +=
+                `<div class="col-lg-3 col-xs-7 col-lg-offset-0 col-xs-offset-2 toRight">
+                    <h1 class="subHeaderLower">${data[i].user_name}</h1>
+                    <p class="captionLower">${data[i].comment}</p>
+                </div>
+                <div class="col-xs-1"></div>`;
+        }
+
+        $('#recentReviews').html(recentReviews);
+
+        if(!showPosts) {
+            $('#feedPosts').hide();
+        } else{
+            $('#feedPosts').show();
+        }
+
+        setTimeout(getComments, 10000);
+    });
+}
+
 jQuery(document).ready(($) => {
     $('.live-search-list li').each(function () {
         $(this).attr('data-search-term', $(this).text().toLowerCase());

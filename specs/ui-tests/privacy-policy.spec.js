1b22df32f46152d4d19a92b22b86fa795105802e
elliebobellie <23409382+elliebobellie@users.noreply.github.com>
2018-04-19 14:30:01 +0100

    more fix
    privaxy policy

--- specs/ui-tests/privacy-policy.spec.js
+++ specs/ui-tests/privacy-policy.spec.js
@@ -5,10 +5,6 @@ describe('uni-bites about tests', function() {
     beforeEach(function(){
     browser.waitForAngularEnabled(false);
     browser.get(browser.params.baseUrl + '/privacy-policy'); // rework to page pattern see : http://blog.scottlogic.com/2015/11/06/ProtractorForBeginnersPart1.html
-    
-    cookieHeader = element(by.id("cookieHeader"));
-    serviceProviderHeader = element(by.id("serviceProviderHeader"));
-    securityHeader = element(by.id("securityHeader"));
 
   });
 
@@ -16,11 +12,6 @@ describe('uni-bites about tests', function() {
     expect(browser.getTitle()).toEqual('Privacy Policy');
   });
 
-  it('should pass if the three paragraphs are there', function(){
-    expect(cookieHeader.getText()).toContain('Cookies');
-    expect(serviceProviderHeader.getText()).toContain('Service Providers');
-    expect(securityHeader.getText()).toContain('Security');
-  });
 
   afterEach(function(){
   });
